1e7674b8e8818e43f740a8eb6a5e8ed6573a506b
ekuo70 <ekuo70@gmail.com>
2020-07-12 13:01:49 -0400

    Add object identifier validator.

--- R/IObject.R
+++ R/IObject.R
@@ -10,7 +10,22 @@ setClass(
 setValidity(
    Class = "IObject",
    method = function(object) {
-      return(TRUE)
+      err <- New.SysMessage()
+      isValid <- Validate(Validator.Length(minLen = 0, maxLen = 1), object@Id)
+      if (isValid != TRUE) {
+         AddMessage(err) <- "Object identifier must be a character string."
+      }
+      if (length(object@Id) == 1) {
+         isValid <- grepl("^[a-zA-Z][a-zA-Z0-9_.]*$", object@Id)
+         if (isValid != TRUE) {
+            AddMessage(err) <- "Invalid object identifier.  An identifier must consist of Consist of A-Z, a-z, 0–9, period (“.”) or underscore ('_') only, and must start with A-Z or a-z."
+         }
+      }
+      if (NoMessage(err)) {
+         return(TRUE)
+      } else {
+         return(GetMessage(err))
+      }
    }
 )
 
