f0e73f7757f122e60917263528c47d7e49421238
ekuo70 <ekuo70@gmail.com>
2020-11-30 17:42:42 -0500

    Minor code changes.

--- R/IPlan.AEnd.R
+++ R/IPlan.AEnd.R
@@ -11,7 +11,8 @@ setClass(
 
 IPlan.AEnd <- function(covYears, premYears = NA,
                        premTable = character(0L), modFactor = c("1" = 1, "2" = 0.5, "4" = 0.25, "12" = 1/12),
-                       polFee = numeric(0), premTaxRate = numeric(0L), matBenSchd, cvTable = character(0L),
+                       polFee = numeric(0), premTaxRate = numeric(0L),
+                       matBenSchd, cvTable = character(0L),
                        commSchd = numeric(0L), ovrdOnPremSchd = numeric(0L), ovrdOnCommSchd = numeric(0L),
                        rein = character(0L), id = character(0L), descrip = character(0L)) {
    plan <- new(
